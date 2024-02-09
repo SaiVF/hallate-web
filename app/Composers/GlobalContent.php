e8010d9cc54ad3054ec8e69bcc15df4f5b2d7543
Santiago Vargas <santiago.vargas@itti.digital>
2024-02-09 01:47:08 -0300

    se actualiza app/composers

--- app/Composers/GlobalContent.php
+++ app/Composers/GlobalContent.php
@@ -9,17 +9,18 @@ use Illuminate\View\View;
 class GlobalContent
 {
   protected $pages;
-  
+  protected $categories;
+
   public function __construct(Page $pages, Category $categories)
   {
     $this->pages = $pages;
     $this->categories = $categories;
   }
-  
+
   public function compose(View $view)
   {
     $productos_categories = $this->categories->where('type', 3)->whereNull('parent')->orderBy('order')->get();
-    
+
     $view->with('productos_categories', $productos_categories);
   }
 }
